b72965e42fbb44462f141e6a1e3d1c9d5d5436dd
marrem97 <58835578Mr>
2017-04-19 21:48:07 +0200

    version1 - probably? formation fix - added non-working footer

--- manager/Start.view.js
+++ manager/Start.view.js
@@ -6,18 +6,34 @@
 		},
 		
 		createContent: function(oController) { // default OpenUI5 function
+            const oView = this;
 			console.log("View called!");
+
+            const oFooter = new sap.m.Bar({
+                contentMiddle: [ new sap.m.Text({ text: "Test" })]
+            });
+
+            oView.oFlexBoxLayout = new sap.m.FlexBox({
+                direction: sap.m.FlexDirection.Column,
+                items: [
+                    oFooter
+                ],
+                justifyContent: sap.m.FlexJustifyContent.Center,
+                alignItems: sap.m.FlexAlignItems.Center,
+                width: "100%"
+            });
 
             const oPage = new sap.m.Page(this.createId("page"), {
                 title: "Account Manager",
                 content: [
-                    // oView.oFlexBoxLayout
+                    oView.oFlexBoxLayout
                 ]
             });
 
-            oPage.addHeaderContent(new sap.m.Button({
-                visible: false
-            }));
+            // is this needed?
+            // oPage.addHeaderContent(new sap.m.Button({
+            //     visible: false
+            // }));
 
             return oPage;
 		}
